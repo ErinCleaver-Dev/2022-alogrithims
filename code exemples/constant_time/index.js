ef5794a9a5dbc0ce12cbd6dd4693c55d7bc0eea1
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-02-03 10:44:45 -0500

    O notation notes

--- code exemples/constant_time/index.js
+++ code exemples/constant_time/index.js
@@ -1,9 +1,21 @@
-let boxes = ["box1", "box2"]
+let boxes = ["box1", "box2", "box3", "box4", "box5", "box6", "box7"]
 
 
-// no matter how many times the boxes are increated it will always be the same
+// no matter how many times the boxes are in the array we always get the first one.  Meaning that the time frame is always the same.  
+
+// Always getting the first item in the array
 function compressFirstBox(boxes) {
     console.log(boxes[0])
 }
 
 compressFirstBox(boxes);
+
+
+// function to get the first two items in the array.
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0])
+    console.log(boxes[1])
+
+}
+console.log(FirstTwoBoxes(boxes))
